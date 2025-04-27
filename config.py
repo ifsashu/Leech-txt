93075680b5d24b99aeda352f2eabaf578964695d
ifsashu <ifsashuabhi@gmail.com>
2025-04-27 14:00:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "25701324"
+api_hash = "1e77a6ec377d28e58247dd22de886d39"
+bot_token = "8098952226:AAGNkjvNQFdn0_t3wj00DGJe__Q7HzO528Y"
+auth_users = [8036182138]
+sudo_users = [8036182138]
